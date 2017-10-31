100b7e1a700fbcd46e1fc013f5616d2f496838da
Mathias Sass Michno <mmichn13@student.aau.dk>
2017-10-31 10:59:21 +0100

    make sloth an object

--- sloth.py
+++ sloth.py
@@ -1,20 +1,54 @@
-import time
 from _sloth import ffi, lib
 
-out = ffi.new('char[512]')
-in_str = input('in str>> ').encode('ascii')
-bits = int(input('bits >> '))
-witness = ffi.new('char[{}]'.format(int(bits/4)))
-iterations = int(input('iterations >> '))
+class Sloth(object):
 
-s = time.time()
-lib.sloth(witness, out, in_str, bits, iterations)
-print("Took {:.2f}s".format(time.time() - s))
-print("witness", ffi.string(witness))
-print("out", ffi.string(out))
+    """Docstring for Sloth. """
+
+    def __init__(self, data=None, bits=2048, iterations=50000):
+        """TODO: to be defined1. """
+        assert isinstance(bits, int)
+        assert isinstance(iterations, int)
+        self._data = None
+        self.data = data
+        self.bits = bits
+        self.iterations = iterations
+        self.witness = None
+        self.final_hash = None
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        if isinstance(value, bytes):
+            self._data = value
+        elif not isinstance(value, str):
+            value = str(value)
+        self._data = value.encode('utf8')
 
 
-s = time.time()
-v = lib.sloth_verification(witness, out, in_str, bits, iterations)
-print("Took {:.2f}s".format(time.time() - s))
-print("VALID" if v else "INVALID!")
+    def compute(self):
+        out = ffi.new('char[512]')
+        witness = ffi.new('char[{}]'.format(int(self.bits/4)))
+
+        lib.sloth(witness, out, self.data, self.bits, self.iterations)
+        self.witness = ffi.string(witness)
+        self.final_hash = ffi.string(out)
+
+    def verify(self):
+        return lib.sloth_verification(self.witness, self.final_hash, self.data, self.bits, self.iterations) == 1
+
+
+if __name__ == "__main__":
+    import time
+    t = time.time()
+    s = Sloth("testy mctest string", iterations=50000)
+    s.compute()
+    print("witness:", s.witness)
+    print("output hash:", s.final_hash)
+    print("Time:", time.time()-t)
+    t = time.time()
+    print("VALID" if s.verify() else "INVALID")
+    print("Time:", time.time()-t)
+
