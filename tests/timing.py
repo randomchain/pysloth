7fbf9038dfe180efb3ee61f56248c3ca7bf2c603
Mathias Sass Michno <m@michno.me>
2018-05-15 11:43:29 +0200

    tweak testing

--- tests/timing.py
+++ tests/timing.py
@@ -6,18 +6,18 @@ TIME_LOG = 'time.log'
 
 INPUT = b'/\x97s\xb8i\xd1\xa5\x84\xcf\x16\x7f\xcc\xc0\xae\xbb\x05!\xf1\x19\x17P\x82\x05\xef\xfcu\x87\x1c"\x10\xaa\xddD\x01\xf4\xcd\xbe\xcf\x87\x05A#\xf6\xaa\xd0Y\xe3\\\xf7\xa8\x08p\x8f\xd8\xfdEi\xdc\xda\xa1l1\x1cE'
 
-BITS = (512, 512 * 2, 512 * 4, 512 * 8)
-ITERATIONS = (500, 1000, 2500, 5000, 10000, 25000, 50000, 75000, 100000)
+BITS = (512, 512 * 2, 512 * 3, 512 * 4, 512 * 5, 512 * 6, 512 * 7, 512 * 8)
+ITERATIONS = (500, 5000, 10000, 25000, 50000, 75000)
 
-def timed_comp(s):
+def timed_task(s, task):
     t_0 = time.time()
-    s.compute()
+    getattr(s, task)()
     s.wait()
     t = time.time() - t_0
     print(t)
     if TIME_LOG:
         with open(TIME_LOG, 'a+') as logf:
-            print(t, file=logf)
+            print("," + str(t), end='', file=logf)
 
 
 def sloth_timing(bits, iterations):
@@ -25,21 +25,26 @@ def sloth_timing(bits, iterations):
                     bits=bits,
                     iterations=iterations)
 
-    timed_comp(s)
-
+    timed_task(s, 'compute')
     assert s.final_hash is not None
+    timed_task(s, 'verify')
+    assert s.valid, "{}\n{}".format(s.final_hash, s.witness)
 
 
 def sloth_timing_configurations(log_file):
     global TIME_LOG
     TIME_LOG = log_file
+    if TIME_LOG:
+        with open(TIME_LOG, 'w') as logf:
+            print("BITS,ITERATIONS,COMPUTATION,VERIFICATION", end='', file=logf)
     for b in BITS:
         for i in ITERATIONS:
             print(b, i)
             if TIME_LOG:
                 with open(TIME_LOG, 'a+') as logf:
-                    print(b, i, file=logf)
+                    print("\n" + str(b) + "," + str(i), end='', file=logf)
             sloth_timing(b, i)
+
 
 if __name__ == "__main__":
     import sys
